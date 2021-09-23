fc0cf277003594ce03680e440fc02cf394347c2a
Thomas Fowler <thomas.fowler@flatironschool.com>
2021-09-23 10:18:50 -0600

    Initial solution.

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,15 @@
-function fibonacci(n) {
-  // type your code here
+const fibonacci = (n, seed = []) => {
+  const tempSeed = seed.length === 0 ? [0, 1] : seed.slice()
+
+  if (n === 0) return tempSeed[0]
+  else if (n === 1) return tempSeed[1]
+
+  if (tempSeed.length <= n) { // n >= 3
+    tempSeed.push(tempSeed[tempSeed.length - 1] + tempSeed[tempSeed.length - 2])
+    return fibonacci(n, tempSeed) 
+  }
+
+  return tempSeed[n]
 }
 
 if (require.main === module) {
@@ -22,3 +32,7 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// if returned array's length is
+// less than `n`, recurse. Otherwise,
+// return arr[n-1]
